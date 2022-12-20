5bfc8b24b2299367def23f6dbd2309e96eb51970
Shaun Hogan <shaun.hogan@dotdigital.com>
2022-12-20 12:55:26 +0200

    Export as Module

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,30 @@ import { ConfigurationCollection } from "./collection/configurtation-collection.
 import { ModuleCallArgument } from "./model/module-call-argument.js";
 import { ArgumentTypes, ConfigurationInterface } from './api/index.js';
 
-function run()
-{
-    const config = new ConfigurationCollection();
-    config.add({
-        key:'regeion',
-        value:"r1"
-    } as ConfigurationInterface)
-    const tracking = new Tracking(config);
-
-
-    tracking.call(CallableTrackingFunctions.create, [
-        new ModuleCallArgument('account_id', 'DM-5862029334-01', ArgumentTypes.STRING)
-    ])
-    tracking.call(CallableTrackingFunctions.track, []);
-      
+export const TrankingModule = {
+    Tracking,
+    CallableTrackingFunctions,
+    ConfigurationCollection,
+    ModuleCallArgument,
+    ArgumentTypes
 }
 
-run();
+
+// function run()
+// {
+//     const config = new ConfigurationCollection();
+//     config.add({
+//         key:'regeion',
+//         value:"r1"
+//     } as ConfigurationInterface)
+//     const tracking = new Tracking(config);
+
+
+//     tracking.call(CallableTrackingFunctions.create, [
+//         new ModuleCallArgument('account_id', 'DM-5862029334-01', ArgumentTypes.STRING)
+//     ])
+//     tracking.call(CallableTrackingFunctions.track, []);
+      
+// }
+
+// run();
